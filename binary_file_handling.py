988ea3e382ac79ea3af04a191938d4033a62a2a5
christopherjneelankavil <christopherneelankavil@gmail.com>
2025-01-27 01:57:15 +0530

    updated code for file handling of binary

---
+++ binary_file_handling.py
@@ -0,0 +1,15 @@
+data = b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'
+# try:
+#     with open('example.bin','wb') as source_file:
+#         source_file.write(data)
+# except FileNotFoundError:
+#     print('File not found')
+
+
+# try: 
+#     with open('example.bin','rb') as source_file:
+#         print(source_file.read())
+
+# except FileNotFoundError:
+#     print('File not found')
+
