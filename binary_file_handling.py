f8050970d0f5079695d052d6c2bb087eb935b9f8
christopherjneelankavil <christopherneelankavil@gmail.com>
2025-01-27 02:08:56 +0530

    updated code for file handling of binary

--- binary_file_handling.py
+++ binary_file_handling.py
@@ -22,3 +22,22 @@ data = b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'
 #         destination_file.write(content)
 # except FileNotFoundError:
 #     print('File not found')
+
+# try :
+#     with open('files\source_file.txt','r') as source_file:
+#         lines = source_file.readlines()
+#         print(f'Number of lines in the file : {len(lines)}')
+#         print(f'Sum of words {sum(len(lines.split()) for lines in lines)}')
+#         print(f'Sum of characters {sum(len(lines) for lines in lines)}')
+
+# except FileNotFoundError:
+#     print('File not found')
+
+
+try : 
+    with open('example.txt','w+') as file:
+        file.write('This is a test file')
+        file.seek(0)
+        print(file.read())
+except FileNotFoundError:
+    print('File not found')
